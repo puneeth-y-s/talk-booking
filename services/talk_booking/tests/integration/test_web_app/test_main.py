912e0244411ee948dddea1a5839c1ccbfe3fde9a
Puneeth Y S <puneeth@Puneeths-MacBook-Pro.local>
2025-07-28 12:29:56 +0530

    Updated the dockerfile

--- services/talk_booking/tests/integration/test_web_app/test_main.py
+++ services/talk_booking/tests/integration/test_web_app/test_main.py
@@ -13,7 +13,7 @@ def test_health_check(client):
     """
     GIVEN
     WHEN health check endpoint is called with GET method
-    THEN response with status 200 and body OK is returned
+    THEN response with status 200 and body OK is returned.
     """
     response = client.get("/health-check/")
     assert response.status_code == 200
