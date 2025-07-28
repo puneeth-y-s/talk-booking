5b431b08d36c9054a223919242081080d86275b3
Puneeth Y S <puneeth@Puneeths-MacBook-Pro.local>
2025-07-28 10:17:23 +0530

    Add code quality job

--- services/talk_booking/tests/integration/test_web_app/test_main.py
+++ services/talk_booking/tests/integration/test_web_app/test_main.py
@@ -8,12 +8,13 @@ from web_app.main import app
 def client():
     return TestClient(app)
 
+
 def test_health_check(client):
     """
     GIVEN
     WHEN health check endpoint is called with GET method
     THEN response with status 200 and body OK is returned
     """
-    response =  client.get("/health-check/")
+    response = client.get("/health-check/")
     assert response.status_code == 200
     assert response.json() == {"message": "OK"}
