caf372fd2d85eab74ff2e80ff73df6101d89fad6
Puneeth Y S <puneeth@Puneeths-MacBook-Pro.local>
2025-07-29 11:36:35 +0530

    Testing tf infra creation

--- infrastructure/global/vpc/main.tf
+++ infrastructure/global/vpc/main.tf
@@ -56,3 +56,19 @@ terraform {
     encrypt        = true
   }
 }
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "terraform-test-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "terraform-test-igw"
+  }
+}
