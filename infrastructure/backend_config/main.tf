a1555c5fa631c5426a5ad112043b39591d6c60e1
Puneeth Y S <puneeth@Puneeths-MacBook-Pro.local>
2025-07-29 19:46:23 +0530

    updated remote state backend

--- infrastructure/backend_config/main.tf
+++ infrastructure/backend_config/main.tf
@@ -42,13 +42,3 @@ resource "aws_dynamodb_table" "terraform_state" {
     type = "S"
   }
 }
-
-terraform {
-  backend "s3" {
-    bucket         = "puneethys-tf-state-1123581321345589"
-    key            = "global/s3/vpc/terraform.tfstate"
-    dynamodb_table = "terraform-state"
-    region         = "us-east-1"
-    encrypt        = true
-  }
-}
--- infrastructure/global/vpc/main.tf
+++ infrastructure/global/vpc/main.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "puneethys-tf-state-1123581321345589"
+    key            = "global/s3/vpc/terraform.tfstate"
+    dynamodb_table = "terraform-state"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
